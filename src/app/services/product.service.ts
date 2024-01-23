23114f9711ed59483443c591f66bdfe6d0e80988
SeddikEsprit <98233814+SeddikEsprit@users.noreply.github.com>
2024-01-23 08:21:38 +0100

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:8080';
+  private apiUrl = 'http://localhost:8090';
 
   constructor(private http: HttpClient) { }
 
