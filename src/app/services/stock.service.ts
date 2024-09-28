e51e16eed8c0c46851566e682f8f0397a5a6faf4
Sami Ben Laribi <98401705+SamiBenLaribi@users.noreply.github.com>
2024-09-28 14:27:32 +0200

    Update stock.service.ts

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class StockService {
-  private apiUrl = 'http://192.168.71.220:8082/stock';
+  private apiUrl = 'http://192.168.1.180:8082/stock';
 
   constructor(private http: HttpClient) {
   }
